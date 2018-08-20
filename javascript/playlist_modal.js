cf270dc23a137f7394c81e06f3094465c7e1ef92
Seba <sebagb2910@gmail.com>
2018-08-20 17:14:40 -0300

    modal playlist

--- javascript/playlist_modal.js
+++ javascript/playlist_modal.js
@@ -7,7 +7,7 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 //var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
+// When the user clicks the button, open the modal
 btn.onclick = function () {
     modal.style.display = "block";
 }
@@ -22,4 +22,30 @@ window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+function createPlaylist() {
+    var url = 'http://localhost:3000/playlists';
+    var playlist = getCreatedPlaylist();
+    var fetchData = {
+        method: 'POST',
+        body: playlist,
+        headers: new Headers()
+    }
+    fetch(url, fetchData)
+    .then(function() {
+        // To Do
+    })
+}
+function getCreatedPlaylist() {
+    return {
+        id: getLastId();
+        name: '',
+        description: '',
+        image: '',
+        songs: []
+    }
+}
+function getLastId() {
+    return 42;
 }
